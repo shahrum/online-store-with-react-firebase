db6166e51eae6f00cba4010f1532e70878bfd3ad
Shahram Hosseinabadi <shahram.hosseinabadi@vocus.com.au>
2020-06-09 16:05:38 +1000

    storing users on firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,33 @@ const config = {
 	measurementId: "G-WKP15QNT0C",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const snapShot = await userRef.get();
+
+	console.log("snapShot:", snapShot);
+	console.log("userAuth:", userAuth);
+
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData,
+			});
+		} catch (error) {
+			console.log("error on user creation", error);
+		}
+	}
+	return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
